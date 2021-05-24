dea3891e6cb03a43e881d50012ed5ebcb9bac811
Ptyca <82829609+Ptyca@users.noreply.github.com>
2021-05-24 19:57:39 +0300

    sum

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const Validation = require('./js/Validation');
 const v = new Validation();
 
 //console.log(v.isValidPhoneNumber(+37061234567), true);
-
 //console.log(v.isValidEmail('demo@example.com'), true);
-
 //console.log(v.isValidMessage('Some message'), true);
 //console.log(v.isValidMessage('qweqweqwqwe'), true);
 //console.log(v.isValidMessage(123123123), false);
@@ -13,10 +11,6 @@ const v = new Validation();
 //console.log(v.isValidFirstName('Vardenis'), true);
 //console.log(v.isValidLastName('Pavardenis'), true);
 //console.log(v.isValidLastName('Pavardenis'), true);
-
-
-
-
 // console.log(v.isValidMonthName('Sausis'), true);
 // console.log(v.isValidMonthName('January'), true);
 // console.log(v.isValidWeekdayName('Pirmadienis'), true);
---
+++ js/sum/sum.test.js
@@ -0,0 +1,18 @@
+const sum = require('./sum');
+
+test('adds 0 + 0 to equal 0', () => {
+  expect(sum(0, 0)).toBe(0);
+});
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+test('adds 0.5 + 0.5 to equal 1', () => {
+    expect(sum(0.5, 0.5)).toBe(1);
+});
+
+test('adds 0.1 + 0.2 to equal 0.3', () => {
+    expect(sum(0.1, 0.2)).toBeCloseTo(0.3, 5);        
+});
+
